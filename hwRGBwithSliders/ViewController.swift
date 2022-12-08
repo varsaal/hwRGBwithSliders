109a0733ee3f5d4bd55d30863312aa89f9d3b930
varshaver <varshaveraleksey@gmail.com>
2022-12-09 02:56:55 +0300

    1. Setup sliders

--- hwRGBwithSliders/ViewController.swift
+++ hwRGBwithSliders/ViewController.swift
@@ -8,10 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    @IBOutlet var blueLabel: UILabel!
+    @IBOutlet var redTextfild: UITextField!
+    @IBOutlet var greenTextfild: UITextField!
+    @IBOutlet var blueTextfild: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Setup sliders
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+        redSlider.maximumTrackTintColor = .gray
+        greenSlider.maximumTrackTintColor = .gray
+        blueSlider.maximumTrackTintColor = .gray
+        redSlider.minimumValue = 0
+        greenSlider.minimumValue = 0
+        blueSlider.minimumValue = 0
+        
+        
     }
 
 
